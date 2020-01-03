efc1b65231aa482dafd3501ab8a2ab65678efa0c
Neil Khemani <radixtwo@users.noreply.github.com>
2020-01-02 22:54:06 -0800

    clarifications
    yea

--- src/play-sim.c
+++ src/play-sim.c
@@ -6,28 +6,29 @@
 #include <stdio.h>
 #include <time.h>
 
+// main loop
 int main() {
     //srand(time(NULL));
-    srand((unsigned)getRandomUInt64());
+    srand((unsigned)getRandomUInt64()); // this is not pseudorandom
     int type;
     printf("%s1) Human vs. Human\n2) Human vs. AI\n3) AI vs. Human\n4) AI vs. AI\n\n1, 2, 3, or 4?: ", ANSI.erase);
     scanf(" %d", &type);
     sim_t *game = sim_init(false, false);
     switch(type) {
-        case 1:
+        case 1: // nothing needs to be changed
             break;
-        case 2:
+        case 2: // player 2 is AI
             game_toggle_ai(game, false, true);
             break;
-        case 3:
+        case 3: // player 1 is AI
             game_toggle_ai(game, true, false);
             break;
-        default:
+        default: // both players are AI
             game_toggle_ai(game, true, true);
             break;
     }
-    game_play(game);
-    game_free(game);
+    game_play(game); // game loop
+    game_free(game); // release resources
     return 0;
 }
 
